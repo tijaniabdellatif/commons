8144ddc391f9f2757e0bd8eb731b8144a2558ed3
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2025-01-24 17:29:13 +0100

    build version 0.0.7

--- src/index.ts
+++ src/index.ts
@@ -98,3 +98,4 @@ export { IEmailLocals } from './Interfaces/email.interface';
 export { DatabaseFactory } from './Connection/DatabaseFactory';
 export { CloudinaryService } from './Services/CloudinaryService';
 export { BaseRepository } from './Repositories/BaseRepository';
+export { ConfigSchema, ConfigService } from './Config/ConfigService';
