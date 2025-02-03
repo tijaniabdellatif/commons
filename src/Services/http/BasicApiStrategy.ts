ba4cb6701a4a4f96353bc4b9d2533bb79ae6f1c9
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2025-02-03 01:11:53 +0100

    stabilization Api Axios Strategy

--- src/Services/http/BasicApiStrategy.ts
+++ src/Services/http/BasicApiStrategy.ts
@@ -1,19 +1,15 @@
-import axios,{AxiosInstance} from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { IApiStrategy } from './IApiStrategy';
 
-
 export class BasicApiStrategy implements IApiStrategy {
-
-    public createInstance(baseUrl: string, serviceName?: string) {
-          return axios.create({
-             baseURL:baseUrl,
-             headers:{
-
-                "Content-Type":"application/json",
-                Accept:'application/json',
-                withCredentials: true
-
-             }
-          })
-    }
+  public createInstance(baseUrl: string, serviceName?: string) {
+    return axios.create({
+      baseURL: baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      withCredentials: true,
+    });
+  }
 }
